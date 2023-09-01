c9d2f1606760c1e52dcd61b4510a6d2b2c3e960f
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-09-01 11:12:59 +0100

    added route to bookvisit

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,4 +17,47 @@ exports.addUser = async (req, res) => {
   }
 };
 
+//BOOKINGS
 
+exports.booking = async (req,res)=>{
+  const { email, date } = req.body
+  const { id } = req.params;
+
+  try{
+    const booking = await prisma.user.findUnique({
+      where: {email:email},
+      select: {bookedVisits: true}
+
+    })
+    if(booking.bookedVisits.some((visit)=>visit.id === id)){
+      res.status(400).json({message:"your visit has been  already been booked"})
+    }else{  
+          await prisma.user.update({
+            where:{email: email},
+            data: {
+              bookedVisits:{push:{id, date}},
+            },
+          });
+          res.send("booking confirmed succesfully");
+    }
+  }
+  catch(err){
+    console.log(err.message);
+  }
+}
+
+//all bookings
+
+exports.getAllBookings = async (req,res)=>{
+  const { email } = req.body
+  try{
+    const allbookings = await prisma.user.findUnique({
+      where: {email: email},
+      select: {bookedVisits: true}
+    })
+    res.status(201).send(allbookings);
+  }
+  catch(err){
+    console.log(err.message);
+  }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const usercontroller = require("../controllers/user-controller.js")
-const propertycontroller = require("../controllers/property-controller.js")
 const router = express.Router();
 
 router.route('/signup').post(usercontroller.addUser)
+router.route('/bookvisit/:id').post(usercontroller.booking)
+router.route('/bookings').post(usercontroller.getAllBookings)
 
 module.exports = router;
